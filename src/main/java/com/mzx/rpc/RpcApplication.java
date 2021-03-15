f0cbea6386aa69fccc9bc10aa4980648303bc355
mazhenxin1998 <2280480546@qq.com>
2021-03-15 23:05:26 +0800

    init

--- src/main/java/com/mzx/rpc/RpcApplication.java
+++ src/main/java/com/mzx/rpc/RpcApplication.java
@@ -1,9 +1,14 @@
 package com.mzx.rpc;
 
 /**
+ * 服务端将服务端接口暴露在注册中心，客户端只需要从注册中心中进行RPC远程调用请求接口.
+ *
  * @author mazhenxin.
  * @slogan 脚踏实地向前看.
  * @create 2021-03-15 22:51 周一.
  */
 public class RpcApplication {
+
+
+
 }
